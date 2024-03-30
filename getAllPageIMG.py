54f5ccb9d2da0e306af1c808e6f81aa3eb233499
nanako <961870173@qq.com>
2024-03-30 23:39:28 +0800

    cmd輸入網址爬取

--- getAllPageIMG.py
+++ getAllPageIMG.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import os
 import requests as axios
@@ -6,16 +7,16 @@ from requests.adapters import HTTPAdapter
 
 import agent
 
-url = "https://ccork.xxtt.info/2024/03/06/tao-liang-a-zhai-bad-jk.html"
-img_ext = '.png'
+url = sys.argv[1] #目标地址
+img_ext = '.png' #保存格式
 proxies = {
     'http': '127.0.0.1:7890',
     'https': '127.0.0.1:7890'
-}
-img_count = 1
-headers = agent.get_agent()
+} #代理ip和端口
+img_count = 1 #图片计数器
+headers = agent.get_agent() #固定请求头
 
-sess = axios.Session()
+sess = axios.Session() #配置会话的最大重试次数
 sess.mount('http://', HTTPAdapter(max_retries=3))
 sess.mount('https://', HTTPAdapter(max_retries=3))
 sess.keep_alive = False  # 关闭多余连接
@@ -39,22 +40,23 @@ def get_each_page_img(img_link_list:[str],file_path:str):
 
 
 htmls = axios.get(url, headers=headers, proxies=proxies)
+tree = html.fromstring(htmls.text) #tree是html组件树
 
-tree = html.fromstring(htmls.text)
+page_link = tree.xpath('//a[@class="page-numbers"]') #正则筛选标签
+page_link_list = [link_obj.attrib.get('href') for link_obj in page_link] #选出剩下页数的链接
 
-page_link = tree.xpath('//a[@class="page-numbers"]')
-page_link_list = [link_obj.attrib.get('href') for link_obj in page_link]
-
-links = tree.xpath('//a[img]/@href')
+links = tree.xpath('//a[img]/@href') #正则筛选图片链接
 links.pop()
 
 path = './assets/'+'-'.join(str(links[0]).split('/')[-1].split('.')[0].split('-')[:-1])+'/'
 if not os.path.exists(path):
-    os.makedirs(path)
+    os.makedirs(path) #生成保存路径
 
-get_each_page_img(links,path)
+get_each_page_img(links,path) #读出当前页面
 
 for page in page_link_list:
+    htmls = axios.get(page, headers=headers, proxies=proxies) #循环get剩下页面的图片
+    tree = html.fromstring(htmls.text)
     links = tree.xpath('//a[img]/@href')
     links.pop()
     get_each_page_img(links,path)
